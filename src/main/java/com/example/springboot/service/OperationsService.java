a02bc03af4a9c5e93f529a3b69ddeead8fd4cc93
Pepe Mo <pepe0910@hotmail.com>
2022-05-13 10:19:01 +0200

    creacion de la interfaz de OperationsService

---
+++ src/main/java/com/example/springboot/service/OperationsService.java
@@ -0,0 +1,11 @@
+package com.example.springboot.service;
+
+import java.math.BigInteger;
+
+public interface OperationsService {
+
+    BigInteger suma(BigInteger numA, BigInteger numB);
+
+    BigInteger resta(BigInteger numA, BigInteger numB);
+
+}
