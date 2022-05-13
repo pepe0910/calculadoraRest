a04bd465a0d93f8c84c0b4721eb2c66c72f36650
Pepe Mo <pepe0910@hotmail.com>
2022-05-13 11:11:03 +0200

    add lib tracer

--- src/main/java/com/example/springboot/controller/calculadoraApiController.java
+++ src/main/java/com/example/springboot/controller/calculadoraApiController.java
@@ -2,6 +2,7 @@ package com.example.springboot.controller;
 
 import com.example.springboot.model.OperacionEnum;
 import com.example.springboot.service.OperationsService;
+import io.corp.calculator.TracerImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,17 +18,21 @@ public class calculadoraApiController implements calculadoraApi {
     @Autowired
     OperationsService operationsService;
 
+    private TracerImpl tracer = new TracerImpl();
+
     @Override
     public ResponseEntity<BigInteger> calcular(BigInteger primerNumero, BigInteger segundoNumero, String operacion) {
 
         BigInteger    resultado     = null;
         OperacionEnum operacionEnum = OperacionEnum.valor(operacion);
 
+
         if (OperacionEnum.SUMA.equals(operacionEnum)) {
             resultado = operationsService.suma(primerNumero, segundoNumero);
         } else if (OperacionEnum.RESTA.equals(operacionEnum)) {
             resultado = operationsService.resta(primerNumero, segundoNumero);
         }
+        tracer.trace(resultado);
 
         return new ResponseEntity<>(resultado, HttpStatus.OK);
     }
